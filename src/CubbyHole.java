1f86e80cf2209a97a98cd14810c727ad19ea6fba
Tiago Mateus Carvalho <tiagomc123@hotmail.com>
2022-10-03 19:31:51 -0300

    changes

--- src/CubbyHole.java
+++ src/CubbyHole.java
@@ -3,12 +3,12 @@ public class CubbyHole {
 	private int content  = -1;
 	
 	public int get(int consumer) {
-			System.err.printf("Cosumidor %d consumiu %d \n", consumer, content);
+			System.err.printf("Cosumidor |%d| consumiu [%d] \n", consumer, content);
 			return content;
 	}
 	
 	public void set(int producer, int value) {
 			content = value;
-			System.out.printf("Pordutor %d produziu %d \n", producer, content);
+			System.out.printf("Pordutor |%d| produziu [%d] \n", producer, content);
 	}
 }
